98184496c6864c61b2c1c553f337bf315dc4913f
#TheDayG0ne <thedayg0ne@internet.ru>
2023-07-30 12:43:27 +0700

    Update exit command: add .confetti argument
    Обновление команды exit: добавление аргумента .confetti

--- src/commands/exit.js
+++ src/commands/exit.js
@@ -1,5 +1,9 @@
 export default class ExitCommand {
     execute(term, params, directory, setDirectory) {
+        if (params[1] === ".confetti") {
+             // create your logic here
+            return;
+        } 
         term.writeln('');
         term.writeln('The NE-DOS session will be terminated in 5 seconds. Bye-Bye!');
   
