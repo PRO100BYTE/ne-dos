74b4f5c2785d24059fefdeb01b0c5ac8124815f8
#TheDayG0ne <thedayg0ne@internet.ru>
2023-07-30 13:27:32 +0700

    Remove .team and .github

--- src/commands/ver.js
+++ src/commands/ver.js
@@ -14,25 +14,6 @@ export default class VersionCommand {
 
       return;
     }
-    if (params[1] === ".team") {
-      term.writeln('');
-      term.writeln(`NE-DOS ${window['VERSION']}`);
-      term.writeln('');
-      term.writeln('Developed by PRO100BYTE Team:');
-      term.writeln('Eduard Ilin (edwardcode)');
-      term.writeln('Evgeniy Struchkov (thedayg0ne)');
-      term.writeln('');
-      term.writeln('You can find more information in the GitHub repository:');
-      term.writeln('https://github.com/PRO100BYTE/ne-dos or print "ver .github"')
-      return;
-    }
-    if (params[1] === ".github") {
-      term.writeln('')
-      term.writeln('Opening GitHub Repository...')
-      window.open(`https://github.com/PRO100BYTE/ne-dos`, '_blank');
-      
-      return;
-    }
     term.writeln('');
     term.writeln(`NE-DOS ${window['VERSION']}`);
     term.writeln('');
