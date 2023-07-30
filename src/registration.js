f6706d1e107d0f039af9d5d61a821bb732148497
#TheDayG0ne <thedayg0ne@internet.ru>
2023-07-30 22:50:45 +0700

    Add 'command' command

--- src/registration.js
+++ src/registration.js
@@ -18,6 +18,7 @@ import MakeDirectoryCommand from "./commands/Filesystem/mkdir";
 import CatCommand from "./commands/Filesystem/cat";
 import ECodeAPICommand from "./commands/ECodeAPI/ecode";
 import GeoCommand from "./commands/Utility/geo";
+import CommandCOMCommand from "./commands/System/command";
 
 export const registerAllCommands = () => {
   let registeredCommands = {};
@@ -26,6 +27,7 @@ export const registerAllCommands = () => {
   registeredCommands['ver'] = new VersionCommand();
   registeredCommands['cls'] = new ClearCommand();
   registeredCommands['wait'] = new WaitCommand();
+  registeredCommands['command'] = new CommandCOMCommand();
 
   // Filesystem commands
   registeredCommands['dir'] = new DirectoryCommand();
