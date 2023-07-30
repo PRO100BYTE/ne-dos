08f74ca61d63fdf7f04658a169a6cab81cce28ff
Edward <eduard@dpkg.su>
2023-07-30 09:31:20 +0300

    Update exit.js
    Настолько ужасного кода я еще не видел

--- src/commands/exit.js
+++ src/commands/exit.js
@@ -2,21 +2,21 @@ import JSConfetti from 'js-confetti'
 
 export default class ExitCommand {
     execute(term, params, directory, setDirectory) {
-            if (params[1] === ".confetti") {
-                const jsConfetti = new JSConfetti() 
-                jsConfetti.addConfetti({ emojis: ['🖥', '💻'], emojiSize: 50, confettiNumber: 100, })
-                term.writeln(''); 
-                term.writeln('The NE-DOS session will be terminated in 5 seconds. Bye-Bye!');
-                setTimeout(() => {
-                    window.close();
-                }, 5000);
-               return;
-           } 
-           term.writeln('');
-           term.writeln('The NE-DOS session will be terminated in 5 seconds. Bye-Bye!');
+        term.writeln('');
+        term.writeln('The NE-DOS session will be terminated in 5 seconds. Bye-Bye!');
+        if (params[1] === ".confetti") {
+            const jsConfetti = new JSConfetti() 
+            jsConfetti.addConfetti({ emojis: ['🖥', '💻'], emojiSize: 50, confettiNumber: 100, })
+            term.writeln(''); 
+            term.writeln('The NE-DOS session will be terminated in 5 seconds. Bye-Bye!');
+            setTimeout(() => {
+                window.close();
+            }, 5000);
+            return;
+        }
 
-         setTimeout(() => {
-           window.close();
-         }, 5000);
-       }
-    }       
+        setTimeout(() => {
+            window.close();
+        }, 5000);
+    }
+}
