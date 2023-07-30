66eca647a58ea7d13726c34e89b83be36c6c748b
#TheDayG0ne <thedayg0ne@internet.ru>
2023-07-30 21:16:20 +0700

    Update storeName

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ BrowserFS.install(window);
 BrowserFS.configure({
   fs: "LocalStorage",
   options: {
-    storeName: "NE-DOS"
+    storeName: "NEDOS"
   }
 }, () => {
   window.fs = window.require("fs");
