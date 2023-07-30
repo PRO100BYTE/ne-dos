f92e0f2ca335e4a5fe6c270c2f8b4b17fac73b23
#TheDayG0ne <thedayg0ne@internet.ru>
2023-07-31 00:10:28 +0700

    Add weather command

---
+++ src/commands/Utility/weather.js
@@ -0,0 +1,47 @@
+export default class WeatherCommand {
+    execute(term, params, directory, setDirectory) {
+      if (params.length === 0) {
+        term.writeln("weather: takes the name of a city as an argument and prints the current weather in it.");
+        term.writeln("Example: weather Moscow");
+        return;
+      }
+
+      let city = params.join(" ");
+      let url = `https://wttr.in/${city}?format=j1`;
+      
+      fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("HTTP error " + response.status);
+          }
+        })
+        .then(data => {
+          term.writeln(`Temperature: ${data.current_condition[0].temp_C} °C`);
+          term.writeln(`Feels like: ${data.current_condition[0].FeelsLikeC} °C`);
+          term.writeln(`Humidity: ${data.current_condition[0].humidity} %`);
+          term.writeln(`Pressure: ${data.current_condition[0].pressure} mmHg`);
+          term.writeln(`Wind speed: ${data.current_condition[0].windspeedKmph} km/h`);
+          term.writeln(`Wind direction: ${data.current_condition[0].winddir16Point}`);
+          term.writeln(`Cloud cover: ${data.current_condition[0].cloudcover} %`);
+          term.writeln(`Precipitation: ${data.current_condition[0].precipMM} mm`);
+          term.writeln(`Visibility: ${data.current_condition[0].visibility} km`);
+          term.writeln(`Weather conditions: ${data.current_condition[0].weatherDesc[0].value}`);
+        })
+        .catch(error => {
+          term.writeln("Error: Failed to get weather data");
+        });
+    }
+    
+    description() {
+      return "Prints the current weather in the specified city";
+    }
+    
+    help(term) {
+      term.writeln("Usage: weather [city]");
+      term.writeln("Takes the name of a city as an argument and prints the current weather in it.");
+      term.writeln("Example: weather Moscow");
+    }
+  }
+  
--- src/registration.js
+++ src/registration.js
@@ -22,6 +22,7 @@ import RegisterCmdCommand from "./commands/System/registercommand";
 import CommandCOMCommand from "./commands/System/command";
 import DeleteCommand from "./commands/Filesystem/rm";
 import DeleteDirectoryCommand from "./commands/Filesystem/rmdir";
+import WeatherCommand from "./commands/Utility/weather";
 
 export const registerAllCommands = () => {
   let registeredCommands = {};
@@ -56,6 +57,7 @@ export const registerAllCommands = () => {
   registeredCommands['ip'] = new IpCommand();
   registeredCommands['geoip'] = new GeoIpCommand();
   registeredCommands['geo'] = new GeoCommand();
+  registeredCommands['weather'] = new WeatherCommand();
 
   // ECodeAPI
   registeredCommands['ecode'] = new ECodeAPICommand();
