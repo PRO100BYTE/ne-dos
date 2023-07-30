a5b6800c18f8e5cdf2a09aa5730e407216e4d4f0
#TheDayG0ne <thedayg0ne@internet.ru>
2023-07-30 13:04:52 +0700

    Update exit

--- src/commands/exit.js
+++ src/commands/exit.js
@@ -1,14 +1,22 @@
+import JSConfetti from 'js-confetti'
+
 export default class ExitCommand {
     execute(term, params, directory, setDirectory) {
-        if (params[1] === ".confetti") {
-             // create your logic here
-            return;
-        } 
-        term.writeln('');
-        term.writeln('The NE-DOS session will be terminated in 5 seconds. Bye-Bye!');
-  
-      setTimeout(() => {
-        window.close();
-      }, 5000);
-    }
-  }  
+            if (params[1] === "/confetti") {
+                const jsConfetti = new JSConfetti() 
+                jsConfetti.addConfetti({ emojis: ['🖥', '💻'], emojiSize: 50, confettiNumber: 100, })
+                term.writeln(''); 
+                term.writeln('The NE-DOS session will be terminated in 5 seconds. Bye-Bye!');
+                setTimeout(() => {
+                    window.close();
+                }, 5000);
+               return;
+           } 
+           term.writeln('');
+           term.writeln('The NE-DOS session will be terminated in 5 seconds. Bye-Bye!');
+
+         setTimeout(() => {
+           window.close();
+         }, 5000);
+       }
+    }       
