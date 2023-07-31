1e670b227c71c8b7c5aad1a3a885786738095c87
#TheDayG0ne <thedayg0ne@internet.ru>
2023-07-31 16:19:15 +0700

    Update calc command

--- src/commands/Utility/calc.js
+++ src/commands/Utility/calc.js
@@ -1,46 +1,29 @@
+const basicMath = require('advanced-calculator');
+
 export default class CalcCommand {
-    execute(term, params, directory, setDirectory) {
-        function calc(args) {
-            if (args.length === 0) {
-              // Выводим справку об использовании, если нет аргументов
-              term.writeln("Usage: calc <expression>");
-              term.writeln("Example: calc 2 + 3 * 5");
-              term.writeln("Supported operations: +, -, *, /");
-              term.writeln("Note: Make sure to put spaces between each operand and operator.");
-            } 
-            else {
-              try {
-                // Приводим аргументы к строке и выполняем вычисление с помощью eval
-                const result = eval(args.join(' '));
-                // Выводим результат
-                term.writeln(result);
-              } catch (error) {
-                // Если возникла ошибка при вычислении, выводим сообщение об ошибке
-                term.writeln('Error: Invalid expression. Please provide a valid mathematical expression.');
-              }
-            }
-          }
-          
-          // Получаем аргументы командной строки, исключая путь к файлу и название программы
-          const args = process.argv.slice(2);
-          
-          // Вызываем калькулятор с переданными аргументами
-          calc(args);
-          
-    }
-    
-    description() {
-      // Краткое описание команды для команды help
-      return "Calculates mathematical expressions.";
-    }
-    
-    help(term) {
-      // Выводим справку об использовании команды calc
+  execute(term, params, directory, setDirectory) {
+    if (params.length > 0) {
+      try {
+        const expression = params[0].replace(/calc|\s/g, '');
+        const result = basicMath.evaluate(expression);
+        term.writeln(result);
+      } catch (error) {
+        term.writeln("Invalid expression: " + error.message);
+      }
+    } else {
       term.writeln("Usage: calc <expression>");
-      term.writeln("Example: calc 2 + 3 * 5");
-      term.writeln("Supported operations: +, -, *, /");
-      term.writeln("Note: Make sure to put spaces between each operand and operator.");
+      term.writeln("Example: calc 2+2*2");
     }
   }
   
+  description() {
+    return "A calculator command that evaluates arithmetic expressions";
+  }
   
+  help(term) {
+    term.writeln("The calc command evaluates arithmetic expressions using the advanced-calculator library.");
+    term.writeln("You can use parentheses, operators (+, -, *, /, %, ^), constants (e, pi), and functions (sin, cos, tan, ln, log, sqrt) in your expressions.");
+    term.writeln("Usage: calc <expression>");
+    term.writeln("Example: calc (2+2)*2^2");
+  }
+}
