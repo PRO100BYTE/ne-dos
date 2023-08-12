bf33b5d7f641557741c84381e7ba917c61881d89
Evgeniy Struchkov <thedayg0ne@internet.ru>
2023-08-12 17:18:08 +0700

    Update base64 command

--- src/commands/Utility/base64.js
+++ src/commands/Utility/base64.js
@@ -1,23 +1,45 @@
+import { Buffer } from 'buffer';
+
 export default class Base64Command {
-    execute(term, params, directory, setDirectory) {
-      if (params.length === 1) {
-        this.help(term);
-      } else {
-        let fullText = params.join(" ");
-        let text = fullText.replace("base64 ", "");
-        let encoded = btoa(text);
-        term.writeln(`${encoded}`);
-      }
+  execute(term, params, directory, setDirectory) {
+    if (params.length === 1) {
+      this.help(term);
+      return;
     }
     
-    description() {
-      return "Encodes the text using base64 and outputs the result to the terminal";
+    const action = params[1];
+    const text = params.slice(2).join(' ');
+    
+    if (action !== 'encode' && action !== 'decode') {
+      term.writeln('Invalid action: ' + action);
+      term.writeln('Usage: base64 encode <text> or base64 decode <base64 text>');
+      return;
     }
     
-    help(term) {
-        term.writeln("Usage: base64 <text>");
-        term.writeln("Encodes text using base64 and outputs the result to the terminal");
-        term.writeln("Text may contain spaces or commas");
+    if (!text || typeof text !== 'string') {
+      term.writeln('Invalid text: ' + text);
+      term.writeln('Usage: base64 encode <text> or base64 decode <base64 text>');
+      return;
     }
+    
+    let result;
+    if (action === 'encode') {
+      result = Buffer.from(text, 'utf-8').toString('base64');
+    } else {
+      result = Buffer.from(text, 'base64').toString('utf-8');
+    }
+    
+    term.writeln(result);
   }
   
+  description() {
+    return "Encode or decode text in base64 format";
+  }
+  
+  help(term) {
+    term.writeln('Usage: base64 encode <text> or base64 decode <base64 text>');
+    term.writeln('Example: base64 encode NE-DOS');
+    term.writeln('Result: TkUtRE9T');
+  }
+  
+}
