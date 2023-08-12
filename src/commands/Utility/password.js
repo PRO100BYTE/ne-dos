3b165c645a599b61bf1cc5514cd56c5bd160f751
Evgeniy Struchkov <thedayg0ne@internet.ru>
2023-08-12 20:50:53 +0700

    Update password command
    Теперь используется crypto-js. Также, минимальная длина пароля - 12 символов

--- src/commands/Utility/password.js
+++ src/commands/Utility/password.js
@@ -1,25 +1,31 @@
+var CryptoJS = require("crypto-js");
+
 export default class PasswordCommand {
-    execute(term, params, directory, setDirectory) {
-      let length = 8;
-      if (params[1] && Number.isInteger(+params[1])) {
-        length = +params[1];
-      }
-      let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@()+-=;:,.?";
-      let password = "";
-      for (let i = 0; i < length; i++) {
-        password += chars[Math.floor(Math.random() * chars.length)];
-      }
-      term.writeln("Your password is: " + password);
+  execute(term, params, directory, setDirectory) {
+    let length = 12;
+    if (params[1] && Number.isInteger(+params[1])) {
+      length = +params[1];
     }
-    
-    description() {
-      return "Generate a random password of a given length";
+    if (length < 12) {
+      term.writeln("Invalid password length. It should be at least 12.");
+      return;
     }
-    
-    help(term) {
-      term.writeln("Usage: password [length]");
-      term.writeln("Generate a random password of a given length. If the argument is not specified, an 8-character password will be generated.");
-      term.writeln("Example: password 12");
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,./<>?";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      let byte = CryptoJS.lib.WordArray.random(1).toString().charCodeAt(0);
+      password += chars[byte % chars.length];
     }
+    term.writeln("Your password is: " + password);
   }
   
+  description() {
+    return "Generate a random password of a given length of at least 12";
+  }
+  
+  help(term) {
+    term.writeln("Usage: password [length]");
+    term.writeln("Generate a random password of a given length of at least 12");
+    term.writeln("Example: password 16");
+  }
+}
