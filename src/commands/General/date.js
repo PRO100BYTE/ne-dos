d511a29474164959d02b8afa9f6ec46b8a0f2f71
Evgeniy Struchkov <thedayg0ne@internet.ru>
2023-12-02 07:48:07 +0700

    Update date command

--- src/commands/General/date.js
+++ src/commands/General/date.js
@@ -1,15 +1,21 @@
 import dateFormat from "dateformat";
-const date = new Date();
-const d = dateFormat(date, "ddd m-dd-yyyy");
 
 export default class DateCommand {
     execute(term, params, directory, setDirectory) {
+        const date = new Date();
+        const d = dateFormat(date, "ddd m-dd-yyyy");
         term.writeln('');
         term.writeln(`Current date is ${d}`);
         term.writeln('');
     }
 
     description() {
-        return "Return current date";
+        return "Print current date";
+    }
+
+    help(term) {
+        term.writeln("Usage: date")
+        term.writeln("This command prints the current date in the format ddd m-dd-yyyy")
+        term.writeln("Sample output: Thu 01-01-1970") // Current date is Thu 01-01-1970
     }
 }
